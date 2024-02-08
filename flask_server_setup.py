eb9476f8036b1182ce02254f4d81ad997c11f0a1
Zoe Zelkha <zzelkha@gmail.com>
2024-02-08 09:19:30 +0100

    added flask CORS

--- flask_server_setup.py
+++ flask_server_setup.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import yaml
 from chitchat_bot import Chatbot
 import uuid
@@ -11,6 +12,7 @@ bots = {}
 
 # Initialize Flask app
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
